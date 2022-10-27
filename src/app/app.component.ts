2317d6bddb3531a1936be1d254abef50ada3cace
Andries <nando.chessplayer@gmail.com>
2022-10-27 23:01:06 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  date: string = '';
+  amount: string = '';
+
+  onNameChange(event: any) {
+    this.name = event.target.value;
+  }
+
+  onDateChange(event: any) {
+    this.date = event.target.value;
+  }
+
+  onAmountChange(event: any) {
+    if (!isNaN(event.target.value)) {
+      this.amount = event.target.value;
+    }
+  }
 }
